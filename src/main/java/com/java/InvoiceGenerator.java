d26610c73204b71e521320bc9fb879b127115ed2
Ranidhumma <ranidhumma8@gmail.com>
2022-03-24 18:38:56 +0530

    calculating Total fare

--- src/main/java/com/java/InvoiceGenerator.java
+++ src/main/java/com/java/InvoiceGenerator.java
@@ -2,9 +2,16 @@ package com.java;
 
 public class InvoiceGenerator {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to cab Invoice Generartor program");
-	}
+	public double CalculateFare(double distance, int time) {
+		int MINIMUM_COST_PER_KILOMETER = 10;
+		int COST_PER_TIME = 1;
+		int MINIMUM_FARE = 5;
+		double fare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
 
+		if (fare < MINIMUM_FARE) {
+			return MINIMUM_FARE;
+		}
+		return fare;
+
+	}
 }
--- src/test/java/com/java/InvoiceTestServices.java
+++ src/test/java/com/java/InvoiceTestServices.java
@@ -1,4 +1,26 @@
 package com.java;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class InvoiceTestServices {
+
+	@Test
+	public void givenDistanceAndTimeShouldReturnTotalFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 2.0;
+		int time = 5;
+		double fare = invoiceGenerator.CalculateFare(distance, time);
+		Assert.assertEquals(25.00, fare, 0.0);
+	}
+
+	@Test
+	public void givenLessDistanceOrTime_ShouldReturnMinFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 0.1;
+		int time = 1;
+		double fare = invoiceGenerator.CalculateFare(distance, time);
+
+		Assert.assertEquals(2.00, fare, 0.0);
+	}
 }
