3b5732061784b253219c15af6c9b254c46c9d4a3
Ranidhumma <ranidhumma8@gmail.com>
2022-03-24 10:26:51 +0530

    Welcome to cab Service Invoice generator

---
+++ src/main/java/com/java/InvoiceGenerator.java
@@ -0,0 +1,10 @@
+package com.java;
+
+public class InvoiceGenerator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to cab Invoice Generartor program");
+	}
+
+}
